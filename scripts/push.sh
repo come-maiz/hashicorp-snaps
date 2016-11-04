6bb7cff73ece9915dff239f05900ee9888b82b60
Leo Arias <yo@elopio.net>
2016-11-04 12:37:08 -0600

    Push candidate snaps (#6)

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,19 +2,10 @@
 
 set -ev
 
-if [ -z "$SNAPCRAFT_SECRET" ]; then
-    exit 0
-fi
+./scripts/login.sh
 
-mkdir -p ".encrypted"
-if [ ! -e ".encrypted/snapcraft.cfg.enc" ]; then
-    echo "Seeding a new macaroon."
-    echo "$SNAPCRAFT_CONFIG" > ".encrypted/snapcraft.cfg.enc"
-fi
+./scripts/push-candidate.sh $PROJECT
 
-mkdir -p "$HOME/.config/snapcraft"
-openssl enc -aes-256-cbc -base64 -pass env:SNAPCRAFT_SECRET -d -in ".encrypted/snapcraft.cfg.enc" -out "$HOME/.config/snapcraft/snapcraft.cfg"
-
-docker run -v $HOME:/root -v $(pwd)/$1:/cwd snapcore/snapcraft sh -c "cd /cwd; snapcraft push *.snap --release edge"
-
-rm -f "$HOME/.config/snapcraft/snapcraft.cfg"
+# Push to the edge channel.
+docker run -v "${HOME}":/root -v "$(pwd)/$1":/cwd snapcore/snapcraft sh -c "cd /cwd && snapcraft push *master*.snap --release edge"
+rm -f "${HOME}/.config/snapcraft/snapcraft.cfg"
