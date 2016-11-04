8bc5ccff49d62854e09003955bf8f874f5b49b33
Leo Arias <yo@elopio.net>
2016-11-04 13:07:20 -0600

    Fix the push (#7)

--- scripts/push-candidate.sh
+++ scripts/push-candidate.sh
@@ -14,15 +14,15 @@ set -ev
 
 # Check if there is a new tag to push to the candidate channel.
 tmp_dir="$(mktemp -d)"
-source="$(cat "$1"/snapcraft.yaml | grep source | awk '{print $2}'"
-git clone "$source" "${tmp_dir}"
+source="$(cat "$1"/snapcraft.yaml | grep source | awk '{printf $2}')"
+git clone "${source}" "${tmp_dir}"
 last_committed_tag="$(git -C "${tmp_dir}" describe --tags --abbrev=0)"
-docker run -v "${HOME}":/root snapcore/snapcraft sh -c "snapcraft status "$1"-elopio | grep candidate | awk '{print $2}' > tag)"
-last_published_tag="$(cat tag)"
+docker run -v "${HOME}":/root snapcore/snapcraft sh -c "snapcraft status "$1"-elopio > status"
+last_published_tag="$(cat status | grep candidate | awk '{printf $2}')"
 
 if [ "${last_committed_tag}" != "${last_published_tag}" ]; then
     # Build using the latest tag.
     ./scripts/snap-in-docker.sh "$1" "${last_published_tag}"
     # Publish to the candidate channel.
-    docker run -v "${HOME}":/root -v "$(pwd)/$1":/cwd snapcore/snapcraft sh -c "cd /cwd && snapcraft push "*${last_published_tag}*".snap --release candidate"
+    ./scripts/push-in-docker.sh "$1" "${last_published_tag}" candidate
 fi
