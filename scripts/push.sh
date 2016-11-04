bb85cdb387486b1919263705db6aac495b1401dc
Leo Arias <yo@elopio.net>
2016-11-04 23:27:40 +0000

    Make the sed smarter to replace any value

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Push snap updates to the store.
+#
+# Arguments:
+#   project: The name of the project. It must be a directory relative to the
+#            root of the repo.
 
 set -ev
 
@@ -6,6 +12,6 @@ trap "rm -f ${HOME}/.config/snapcraft/snapcraft.cfg" EXIT
 
 ./scripts/login.sh
 
-./scripts/push-candidate.sh $PROJECT
+./scripts/push-candidate.sh $1
 
-./scrips/push-in-docker.sh $PROJECT master edge
+./scrips/push-in-docker.sh $1 master edge
--- scripts/snap.sh
+++ scripts/snap.sh
@@ -13,7 +13,7 @@ apt install software-properties-common -y
 add-apt-repository -y ppa:gophers/archive
 apt update
 cd "$1"
-sed -i "s/source-tag: master/source-tag: '"$2"'/g" snapcraft.yaml
-sed -i "s/version: master/version: '"$2"'/g" snapcraft.yaml
+sed -i "s/source-tag: *$/source-tag: '"$2"'/g" snapcraft.yaml
+sed -i "s/version: *$/version: '"$2"'/g" snapcraft.yaml
 snapcraft clean
 snapcraft
