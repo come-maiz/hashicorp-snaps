a451d9939aa78897d219a9c300cd78c492734390
Leo Arias <yo@elopio.net>
2016-11-04 18:22:28 +0000

    Fix the push to edge

--- scripts/push.sh
+++ scripts/push.sh
@@ -7,5 +7,5 @@ set -ev
 ./scripts/push-candidate.sh $PROJECT
 
 # Push to the edge channel.
-docker run -v "${HOME}":/root -v "$(pwd)/$1":/cwd snapcore/snapcraft sh -c "cd /cwd && snapcraft push *HEAD*.snap --release candidate"
+docker run -v "${HOME}":/root -v "$(pwd)/$1":/cwd snapcore/snapcraft sh -c "cd /cwd && snapcraft push *master*.snap --release edge"
 rm -f "${HOME}/.config/snapcraft/snapcraft.cfg"
