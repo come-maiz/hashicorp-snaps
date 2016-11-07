7b5c93ac9b6a9dc6ccf6a627584f9ae749240dca
Leo Arias <yo@elopio.net>
2016-11-07 23:31:37 +0000

    Try to enable arm without docker

--- scripts/snap-in-docker.sh
+++ scripts/snap-in-docker.sh
@@ -13,7 +13,8 @@ docker_image=snapcore/snapcraft
 
 if [ $2 == 'armhf' ]; then
     docker_image='multiarch/ubuntu-core:armhf-xenial'
-    docker run --rm --privileged multiarch/qemu-user-static:register --reset
+    sudo apt-get --yes --no-install-recommends install binfmt-support qemu-user-static
+    echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' | sudo tee -a /proc/sys/fs/binfmt_misc/register
 fi
 
 docker run -v "$(pwd)":/cwd "$docker_image" sh -c "cd /cwd && ./scripts/snap.sh $1 $3"
